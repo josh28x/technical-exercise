79e63e2b09f0815459088f1160a8ff6756f589d2
josh28x <joshua.programming.java@gmail.com>
2025-04-29 06:09:42 +1000

    Edited package.xml and CMakeLists.txt to declare dependencies

---
+++ src/so100_robot/launch/spawn.launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': open(
+                '/home/joshua/Desktop/ros2_ws/src/so100_robot/urdf/SO_5DOF_ARM100_8j_URDF.SLDASM.urdf').read()}]
+        ),
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=['-entity', 'so100_robot', '-topic', 'robot_description'],
+            output='screen'
+        )
+    ])
+
+
