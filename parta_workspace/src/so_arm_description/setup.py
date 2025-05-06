ef75013183b0e775410c136cd6a675e07be3a232
josh28x <joshua.programming.java@gmail.com>
2025-05-06 21:34:02 +1000

    Added launch files configuration  in setup.py for newly created python launch files to enable ROS2 discovery

--- parta_workspace/src/so_arm_description/setup.py
+++ parta_workspace/src/so_arm_description/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os 
+from glob import glob 
 
 package_name = 'so_arm_description'
 
@@ -10,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path,join('so_arm_description', 'launch', '*.launch.py'))), # addition to tell ROS2 build system to include all added .launch.py files from launch directory when instlaling the so_arm_description package
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('so_arm_description', 'launch', '*.launch.py'))), # addition to tell ROS2 build system to include all added .launch.py files from launch directory when instlaling the so_arm_description package
     ],
     install_requires=['setuptools'],
     zip_safe=True,
