6e8a523efd8079735ef4662bbd4f9e4d4679dfe6
josh28x <joshua.programming.java@gmail.com>
2025-05-08 03:53:27 +1000

    Fixed the core issue of of launch being installed as a Python package

--- parta_workspace/src/so_arm_description/setup.py
+++ parta_workspace/src/so_arm_description/setup.py
@@ -7,7 +7,7 @@ package_name = 'so_arm_description'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
