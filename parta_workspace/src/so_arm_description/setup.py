f073bf3fab021a5ebc3334920c882c0bbbad46e9
josh28x <joshua.programming.java@gmail.com>
2025-05-08 04:01:13 +1000

    Fixed install script path in setup.py

--- parta_workspace/src/so_arm_description/setup.py
+++ parta_workspace/src/so_arm_description/setup.py
@@ -12,7 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('so_arm_description', 'launch', '*.launch.py'))), # addition to tell ROS2 build system to include all added .launch.py files from launch directory when instlaling the so_arm_description package
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
